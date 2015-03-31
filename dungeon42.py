0ba96cb2896b8dcef6e217265dddcb14cc490caf
kulbabba <kulbabba@ukr.net>
2015-03-31 22:17:18 +0300

    random in 3 room is changed

--- dungeon42.py
+++ dungeon42.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import random
 
 if sys.version_info.major > 2:
     # For python 3 and up
@@ -8,6 +9,9 @@ if sys.version_info.major > 2:
 else:
     # Python 2
     dun_input = raw_input
+
+
+chest_content_list = ["empty", "key", "undead", "money"]
 
 class Player:
     # HP - health ponts
@@ -56,7 +60,6 @@ class Player:
 
 class Screen:
     def __init__(self, msg, act, action_cb):
-        print('Class init')
         self.msg = msg
         self.act = act
         self.action = action_cb
@@ -106,33 +109,41 @@ eaction
 
 #-----------------------
 
-def mapSecondRoom(u_input, player):
+def second_room_action(u_input, player):
      print("" + str(u_input))
      if u_input == 1:
-             print ("there is undead, there is a key")
-	     player.have_key(2)
+         print ("there is undead, there is a key")
+         player.have_key(2)
+         player.set_position(mapMainCorridorBegin)
 
-mapSecondRoom = Screen("You see one door in front of you and one door on the right.", "1. Open the door in front of you\n2. Open the door on the left.", end_corridor_action)
+mapSecondRoom = Screen("You can see a chest. Press 1 if you want to open it", " ", second_room_action)
 
 
 #-----------------------
-def third_room_action(u_input, player):
-    print("scr7" + str(u_input))
-    import random
-    foo = [1, 2, 3, 4]
-    random_choice = random.choice(foo)
-    if random_choice == 1:
-         print("The chest is empty.")
-         player.set_position(mapThirdRoom)
-    elif random_choice == 2:
-         print("There is undead. He rises. He attacks you.")
-         player.dec_health()
-    elif random_choice == 3:
-         print ("You are lucky. You have found some money")
-    elif random_choice == 4:
-         print("There is a key.")
-         player.have_key(1)
-         player.set_position(mapMainCorridorBegin)
+# TODO: Write all actions for the chests
+def third_room_choice_action(choice):
+    if choice == "empty":
+        print("The chest is empty")
+    else if choice == "undead":
+        print("Undead")
+    else if choice == "money":
+        print("The chest has money")
+    else if choice == "key":
+	    print("The chest has a key")
+	
+def third_room_action(u_input, player):    
+    if u_input == 1:
+        third_room_choice_action(chest_content_list[0])
+		
+    elif u_input == 2:
+        third_room_choice_action(chest_content_list[1])
+		
+    elif u_input == 3:
+        third_room_choice_action(chest_content_list[2])
+         
+    elif u_input == 4:
+        third_room_choice_action(chest_content_list[3])
+
          
 mapThirdRoom = Screen("You see four chests", "1. Open the first one.\n2.Open the second chest.\n3.Open the third chest.\n4.Open the fourth chest.", third_room_action)
          
@@ -155,7 +166,7 @@ def first_room_action(u_input, player):
          print("The coffin is opened. You see the undead...")
      elif u_input == 2:
          if player.get_key() > 0:
-             print ("scr9")
+             player.set_position(mapSecondRoom)
          else:
              print("the door is closed:(")
 mapFirstRoom = Screen("You can see  dark room. There is coffin.There is one more door.", "1. Open the coffin.\n2. Open the door", first_room_action)
@@ -180,6 +191,7 @@ begin_action)
 def main():
     player = Player(mapMainCorridorBegin) # Put player to default position
     
+    random.shuffle(chest_content_list)
     while player.get_health() > 0:
         screen = player.get_position()
         print('---\n' + screen.msg + '\n---\n\n')
